3a0211d97db4b22ec22b39a6c22210c1c0f4bb60
Michele Fontanella <fontami1@gmail.com>
2025-06-02 11:07:20 +0200

    start to create Bot

---
+++ OtdrBot/src/plot/plotSor.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import json
+import os
+import logging
+
+
+
+class PlotSor (object):
+
+    def create_image_of(self, filename:str):
+        data = {}
+        file_sor = os.environ.get('APP_SOR_FILES') + "/" + filename
+        file_jpg = os.environ.get('APP_JPG_FILES') + "/" + filename.replace(".json", ".jpg")
+        logging.info(f"converting {file_sor} into {file_jpg}")
+        # Load data from JSON file
+        with open(file_sor, 'r') as f:
+            data = json.load(f)
+
+        logging.info(len(data['points']))
+
+        # Extract frequency and power values
+        frequencies = [point["km"] for point in data["points"]]
+        powers = [point["loss"] for point in data["points"]]
+        ml_elements = [point.get("otdr_events", "") for point in data["points"]]
+
+        # Create the plot
+        plt.figure()
+        plt.plot(frequencies, powers)
+
+        # Add labels and title
+        plt.xlabel('Frequency (Hz)')
+        plt.ylabel('Power (dB)')
+        plt.title('Frequency vs Power')
+
+        # Annotate points with non-empty 'ml' labels
+        for i, ml in enumerate(ml_elements):
+            if ml:
+                plt.annotate(ml, (frequencies[i], powers[i]))
+
+        # Save the plot as a JPEG file
+        plt.savefig(file_jpg, format='jpeg')
+        # Set the size of the plot
+        plt.gcf().set_size_inches(10, 6)
+        # Set the size of the window
+        manager = plt.get_current_fig_manager()
+        logging.info(manager.canvas.get_width_height())
+        #manager.window.state('zoomed')
+        manager.resize(1000, 200)
+        logging.info(manager.canvas.get_width_height())
+        # Show the plot
+        #plt.show() 
+
--- OtdrBot/src/testPlot.py
+++ OtdrBot/src/testPlot.py
@@ -3,6 +3,7 @@ import json
 import os
 import time
 import logging
+from plot.plotSor  import  PlotSor
 
 
 
@@ -13,51 +14,7 @@ logging.basicConfig(
     format='%(asctime)s - %(filename)s[%(module)s(%(lineno)s)]: %(levelname)s  %(message)s'  # Log format
 )
 
-
-def create_image_of(filename:str):
-    data = {}
-    file_sor = os.environ.get('APP_SOR_FILES') + "/" + filename
-    file_jpg = os.environ.get('APP_JPG_FILES') + "/" + filename.replace(".json", ".jpg")
-    logging.info(f"converting {file_sor} into {file_jpg}")
-    # Load data from JSON file
-    with open(file_sor, 'r') as f:
-        data = json.load(f)
-
-    logging.info(len(data['points']))
-
-    # Extract frequency and power values
-    frequencies = [point["km"] for point in data["points"]]
-    powers = [point["loss"] for point in data["points"]]
-    ml_elements = [point.get("otdr_events", "") for point in data["points"]]
-
-    # Create the plot
-    plt.figure()
-    plt.plot(frequencies, powers)
-
-    # Add labels and title
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Power (dB)')
-    plt.title('Frequency vs Power')
-
-    # Annotate points with non-empty 'ml' labels
-    for i, ml in enumerate(ml_elements):
-        if ml:
-            plt.annotate(ml, (frequencies[i], powers[i]))
-
-    # Save the plot as a JPEG file
-    plt.savefig(file_jpg, format='jpeg')
-    # Set the size of the plot
-    plt.gcf().set_size_inches(10, 6)
-    # Set the size of the window
-    manager = plt.get_current_fig_manager()
-    logging.info(manager.canvas.get_width_height())
-    #manager.window.state('zoomed')
-    manager.resize(1000, 200)
-    logging.info(manager.canvas.get_width_height())
-    # Show the plot
-    #plt.show() 
-
-
+plotSor = PlotSor()
 # Get directory from environment variable
 watch_dir = os.environ.get('APP_SOR_FILES')
 if not watch_dir:
@@ -76,7 +33,7 @@ while True:
             new_files = current_files - seen_files
             for filename in new_files:
                 logging.info(f"Processing file: {filename}")
-                create_image_of(filename)
+                plotSor.create_image_of(filename)
             seen_files = current_files
             time.sleep(1) 
     except Exception as e:
